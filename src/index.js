c683acbbfbbfef3bc9854239b00fc1700ea43809
Michal Löffler <michal.loffler@vacuumlabs.com>
2022-01-31 15:55:10 +0100

    refactor invoice-related stuff

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
-import contentDisposition from 'content-disposition'
 import express from 'express'
 import bodyParser from 'body-parser'
 import logger from 'winston'
-import pdf from 'html-pdf'
 
 import c from './config'
-import renderInvoice from './invoice'
+import {invoiceHandler} from './invoice'
 import {ACTION_ID_CANCEL, ACTION_ID_SEND_EN, ACTION_ID_SEND_SK, handleAction, handleMessage, initState, boltReceiver, boltApp} from './slack'
 import {initStorage} from './storage'
-import {routes as r, shortNames} from './routes'
+import {routes as r} from './routes'
 
 logger.cli()
 logger.level = c.logLevel
@@ -16,65 +14,7 @@ logger.setLevels(logger.config.npm.levels)
 
 const app = express()
 
-/*const exampleQuery = {
-  invoicePrefix: 'VAC17',
-  invoiceNumber: '0007',
-  vendorName: 'vacuumlabs s.r.o.',
-  vendorAddress: 'Radlinského 10, 81107 Bratislava',
-  vendorID: '48207497',
-  vendorTaxID: '2120112962',
-  vendorVAT: 'SK2120112962',
-  vendorVATPayer: false,
-  vendorIBAN: 'SK8809000000000494005548',
-  vendorBIC: 'GIBASKBX',
-  clientName: 'Samuel Hapák IT',
-  clientAddress: 'Brančská 7, 85105 Bratislava',
-  clientID: '50509527',
-  clientTaxID: '1080786630',
-  clientVAT: 'CZ2120112962',
-  issueDate: Date.parse('2017-11-07'),
-  paymentDate: Date.parse('2017-11-15'),
-  services: [{
-    name: 'Prenájom pracovného priestoru za júl 2017',
-    preTaxCost: 41.67,
-    VATLevel: 0.2,
-    VAT: 8.33,
-    fullCost: 50.00,
-  },
-  {
-    name: 'Odmena za vytvorenie diela',
-    preTaxCost: 100.34,
-    VATLevel: 0.16,
-    VAT: 8.99,
-    fullCost: 3810.10,
-  }],
-  preTaxCostSum: 141.81,
-  VATSum: 19.00,
-  fullCostSum: 151.23,
-  incomingInvoice: true,
-}*/
-
-function query2invoice(query) {
-  const invoice = query.id ? {...shortNames[query.id]} : JSON.parse(query.invoice)
-  invoice.issueDate = Date.parse(invoice.issueDate)
-  invoice.paymentDate = Date.parse(invoice.paymentDate)
-  return invoice
-}
-
-function invoice(req, res) {
-  const invoiceData = query2invoice(req.query)
-  const htmlInvoice = renderInvoice(invoiceData, req.query.lang)
-  pdf
-    .create(htmlInvoice, {format: 'A4'})
-    .toBuffer((err, buffer) => {
-      if (err) logger.warn('PDF conversion failed')
-      const fileName = `${invoiceData.user || invoiceData.clientName}-${invoiceData.invoicePrefix}${invoiceData.invoiceNumber}.pdf`
-      res.setHeader('Content-Disposition', contentDisposition(fileName))
-      res.status(200).send(buffer)
-    })
-}
-
-app.get(r.invoice, bodyParser.urlencoded(), invoice)
+app.get(r.invoice, bodyParser.urlencoded(), invoiceHandler)
 
 app.use(r.events, boltReceiver.router)
 app.use(r.actions, boltReceiver.router)
