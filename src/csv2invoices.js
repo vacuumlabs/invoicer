60edd8fe47a96242026c93a78091e1094bf8bbcd
Alena Poláchová <alena.polachova@vacuumlabs.com>
2022-11-10 13:55:11 +0100

    Change home currency to foreign with currency rate if the money is between two different currencies

--- src/csv2invoices.js
+++ src/csv2invoices.js
@@ -1,5 +1,26 @@
 import parse from 'csv-parse/lib/sync'
 
+const countryToCurrency = {
+  Slovakia: 'EUR',
+  Czechia: 'CZK',
+  Hungary: 'HUF',
+}
+
+// Temporarily hardcoded, these values should be downloaded from this google sheet
+// https://docs.google.com/spreadsheets/d/1HfaU9jXZtCfdotEEfDO0d4EpDLyhmqdepaWgn_a6Lsw/edit#gid=1807750274
+// client:foreign
+const rate = {
+  EUR_EUR: 1,
+  EUR_CZK: 24.4164,
+  EUR_HUF: 404.0329,
+  CZK_CZK: 1,
+  CZK_EUR: 0.0410, // 1/eur_czk
+  CZK_HUF: 16.547603, // eur_huf/eur_czk
+  HUF_HUF: 1,
+  HUF_CZK: 0.060432, // eur_czk/eur_huf
+  HUF_EUR: 0.002475, // 1/eur_huf
+}
+
 const columns = [
   'user', 'slackId', 'email', 'invoicePrefix', 'invoiceNumber', 'relatedInvoice', 'vendorName',
   'vendorStreet', 'vendorCity', 'vendorZip', 'vendorCountry', 'vendorID', 'vendorTaxID',
@@ -20,6 +41,7 @@ function finRound(n) {
 export function csv2invoices(csv) {
   return parse(csv).map((r) => {
     let i = 0
+    let currencyRate
     const row = {services: [], preTaxCostSum: 0, VATSum: 0, fullCostSum: 0}
 
     // copy data of first 30 columns from `r` as object properties of `row`
@@ -43,6 +65,10 @@ export function csv2invoices(csv) {
       row.preTaxCostSum += preTaxCost
       row.VATSum += VAT
       row.fullCostSum += fullCost
+      const clientCurrency = countryToCurrency[row.clientCountry] ?? 'EUR'
+      if (clientCurrency !== row.currency) {
+        currencyRate = rate[`${clientCurrency}_${row.currency}`]
+      }
     }
     // move to next value (after the empty one)
     i++ // 31 if new format
@@ -67,6 +93,7 @@ export function csv2invoices(csv) {
     i++
     // load additional columns
     for (let j = 0; j < additionalColumns.length; i++, j++) row[additionalColumns[j]] = r[i]
+    row.currencyRate = row.currencyRate ?? currencyRate
     return row
   })
 }
