01aecbf3b660e14d961c2929702c11b688281f20
Alena Poláchová <alena.polachova@vacuumlabs.com>
2023-01-15 21:32:54 +0100

    Update maintainer ID

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 export const BLOCK_ID_HOME = 'home_actions'
-// Riso's Slack ID
-export const MAINTAINER_ID = 'UBTKJ1F88'
+// Alenka's Slack ID
+export const MAINTAINER_ID = 'U02C5JCQD34'
 export const FINANCE_BOT_ID = 'UDJ04C1JN'
